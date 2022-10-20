0d30837ed0850e62eb52fd1eb7596856110e0d68
Simon Vargas <sjvargas01@gmail.com>
2022-10-20 17:07:47 -0400

    created service package

---
+++ src/main/java/com/sha/springbootmicroservice2purchase/service/PurchaseService.java
@@ -0,0 +1,12 @@
+package com.sha.springbootmicroservice2purchase.service;
+
+import com.sha.springbootmicroservice2purchase.model.Purchase;
+import com.sha.springbootmicroservice2purchase.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+public interface PurchaseService{
+
+
+}
---
+++ src/main/java/com/sha/springbootmicroservice2purchase/service/PurchaseServiceImpl.java
@@ -0,0 +1,8 @@
+package com.sha.springbootmicroservice2purchase.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PurchaseServiceImpl implements PurchaseService{
+}
