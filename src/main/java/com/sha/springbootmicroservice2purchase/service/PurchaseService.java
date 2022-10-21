d62e66bf8c861fc74f6ad3f5e7a432f60a51b4e9
Simon Vargas <sjvargas01@gmail.com>
2022-10-20 22:51:36 -0400

    created controller, service, and securityconfig files

--- src/main/java/com/sha/springbootmicroservice2purchase/service/PurchaseService.java
+++ src/main/java/com/sha/springbootmicroservice2purchase/service/PurchaseService.java
@@ -5,8 +5,13 @@ import com.sha.springbootmicroservice2purchase.repository.PurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 public interface PurchaseService{
 
 
+    Purchase savePurchase(Purchase purchase);
+
+    List<Purchase> findAllPurchasesOfUser(Long userId);
 }
--- src/main/java/com/sha/springbootmicroservice2purchase/service/PurchaseServiceImpl.java
+++ src/main/java/com/sha/springbootmicroservice2purchase/service/PurchaseServiceImpl.java
@@ -1,8 +1,29 @@
 package com.sha.springbootmicroservice2purchase.service;
 
 
+import com.sha.springbootmicroservice2purchase.model.Purchase;
+import com.sha.springbootmicroservice2purchase.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class PurchaseServiceImpl implements PurchaseService{
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    @Override
+    public Purchase savePurchase(Purchase purchase) {
+
+        purchase.setPurchaseTime(LocalDateTime.now());
+
+        return purchaseRepository.save(purchase);
+
+    }
+    @Override
+    public List<Purchase> findAllPurchasesOfUser(Long userId) {
+        return purchaseRepository.findAllByUserId(userId);
+    }
 }
