cd800c8481ebcc9f6e108c3872f69615b2123ec7
Simon Vargas <sjvargas01@gmail.com>
2022-10-21 22:53:09 -0400

    updated purchase controller

--- src/main/java/com/sha/springbootmicroservice2purchase/controller/PurchaseController.java
+++ src/main/java/com/sha/springbootmicroservice2purchase/controller/PurchaseController.java
@@ -9,18 +9,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/purchase")
+
 public class PurchaseController {
 
     @Autowired
     private PurchaseService purchaseService;
 
+    @RequestMapping("api/purchase")
     public ResponseEntity<?> savePurchase(@RequestBody Purchase purchase)
     {
         return new ResponseEntity<>(purchaseService.savePurchase(purchase), HttpStatus.CREATED);
     }
 
-    @GetMapping("{userId}")
+    @GetMapping(value = "api/purchase/{userId}")
     public ResponseEntity<?> getAllPurchasesOfUser(@PathVariable Long userId) {
         return ResponseEntity.ok(purchaseService.findAllPurchasesOfUser(userId));
     }
